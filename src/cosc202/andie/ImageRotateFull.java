3757d4fe849b4bdbda383d4ba6ba82e79d6c5777
Matthew Rae <raema404@student.otago.ac.nz>
2024-03-16 10:45:55 +1300

    Implement functionality for ImageRotateFull

--- src/cosc202/andie/ImageRotateFull.java
+++ src/cosc202/andie/ImageRotateFull.java
@@ -8,7 +8,7 @@ import java.awt.image.BufferedImage;
  * </p>
  * 
  * <p>
- * A 180 degree Image rotation swaps the width and height of the original 
+ * A 180 degree Image rotation keeps the width and height of the original 
  * image and transposes the pixels accordingly.
  * </p>
  * 
@@ -18,7 +18,28 @@ import java.awt.image.BufferedImage;
 public class ImageRotateFull implements ImageOperation, java.io.Serializable {
 
     public BufferedImage apply(BufferedImage input) {
-        return input;
+        // set output image dimensions to same as input image
+        int outputWidth = input.getWidth();
+        int outputHeight = input.getHeight();
+        
+        int outputType = input.getType();
+        
+        BufferedImage output = new BufferedImage(outputWidth, outputHeight, outputType);
+        
+        // iterate through each pixel of input image
+        for (int iy = 0; iy < input.getHeight(); iy++) {
+            for (int ix = 0; ix < input.getWidth(); ix++) {
+                int argb = input.getRGB(ix, iy);
+            
+                // transpose (x, y) of the input pixel to (x, y) in the output
+                int ox = (output.getWidth() - 1) - ix;
+                int oy = (output.getHeight() - 1) - iy;
+            
+                output.setRGB(ox, oy, argb);
+            }
+        }
+    
+        return output;
     }
     
 }
