4d9c8a341e1629467721a1fae6a32d5d9d2caa44
Matthew Rae <mattyjrae123@hotmail.com>
2024-03-19 12:39:31 +1300

    Add edge checking to include outer edges

--- src/cosc202/andie/MedianFilter.java
+++ src/cosc202/andie/MedianFilter.java
@@ -16,11 +16,11 @@ public class MedianFilter implements ImageOperation, java.io.Serializable {
     }
 
     public BufferedImage apply(BufferedImage input) {
-        BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null), input.isAlphaPremultiplied(), null);
+        BufferedImage output = new BufferedImage(input.getWidth(), input.getHeight(), input.getType());
         int size = (2 * radius + 1) * (2 * radius + 1);
 
-        for (int y = radius; y < input.getHeight() - radius; ++y) {
-            for (int x = radius; x < input.getWidth() - radius; ++x) {
+        for (int y = 0; y < input.getHeight(); ++y) {
+            for (int x = 0; x < input.getWidth(); ++x) {
 
                 int[] neighbourhoodValuesA = new int[size];
                 int[] neighbourhoodValuesR = new int[size];
@@ -29,7 +29,23 @@ public class MedianFilter implements ImageOperation, java.io.Serializable {
 
                 for (int i = -radius, count = 0; i <= radius; i++) {
                     for (int j = -radius; j <= radius; j++, count++) {
-                        int argb = input.getRGB(x + j, y + i);
+                        int xVal = x + j;
+                        int yVal = y + i;
+
+                        // check for x,y outside width,height
+                        if (xVal < 0) {
+                            xVal = 0;
+                        } else if (xVal >= input.getWidth()) {
+                            xVal = input.getWidth() - 1;
+                        }
+
+                        if (yVal < 0) {
+                            yVal = 0;
+                        } else if (yVal >= input.getHeight()) {
+                            yVal = input.getHeight() - 1;
+                        }
+
+                        int argb = input.getRGB(xVal, yVal);
                         int a = (argb & 0xFF000000) >> 24;
                         int r = (argb & 0x00FF0000) >> 16;
                         int g = (argb & 0x0000FF00) >> 8;
