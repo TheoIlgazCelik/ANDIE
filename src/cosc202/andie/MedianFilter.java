c3e56b0c81ecaa8e5c65725a1fb9d1c4865675c5
Matthew Rae <mattyjrae123@hotmail.com>
2024-03-23 11:41:06 +1300

    Comment median filter code

--- src/cosc202/andie/MedianFilter.java
+++ src/cosc202/andie/MedianFilter.java
@@ -3,36 +3,95 @@ package cosc202.andie;
 import java.awt.image.*;
 import java.util.Arrays;
 
+/**
+ * <p>
+ * ImageOperation to apply a Median filter.
+ * </p>
+ * 
+ * <p>
+ * A Median filter blurs an image by replacing each pixel by the median argb values of the
+ * pixels in a surrounding neighbourhood.
+ * </p>
+ * 
+ * @author Josh Saunders
+ * @author Matthew Rae
+ * @version 1.0
+ */
 public class MedianFilter implements ImageOperation, java.io.Serializable {
 
+    /**
+     * The size of filter to apply. A radius of 1 is a 3x3 filter, a radius of 2 a 5x5 filter, and so forth.
+     */
     private int radius;
 
+    /**
+     * <p>
+     * Construct a Median filter with the given size.
+     * </p>
+     * 
+     * <p>
+     * The size of the filter is the 'radius' of the convolution kernel used.
+     * A size of 1 is a 3x3 filter, 2 is 5x5, and so on.
+     * Larger filters give a stronger blurring effect.
+     * </p>
+     * 
+     * @param radius The radius of the newly constructed MedianFilter
+     */
     MedianFilter(int radius) {
         this.radius = radius;
     }
 
+    /**
+     * <p>
+     * Construct a Median filter with the default size.
+     * </p>
+     * 
+     * <p>
+     * By default, a Median filter has radius 1.
+     * </p>
+     * 
+     * @see MedianFilter(int)
+     */
     MedianFilter() {
         this(1);
     }
 
+    /**
+     * <p>
+     * Apply a Median filter to an image.
+     * </p>
+     * 
+     * <p>
+     * Unlike many other filters, the Median filter isn't implemented via convolution.
+     * The neighbourhood values of each pixel are programmatically retrieved, before the median 
+     * is calculated and used to replace the original pixel.
+     * Larger radii lead to stronger blurring.
+     * </p>
+     * 
+     * @param input The image to apply the Median filter to.
+     * @return The resulting (blurred) image.
+     */
     public BufferedImage apply(BufferedImage input) {
         BufferedImage output = new BufferedImage(input.getWidth(), input.getHeight(), input.getType());
         int size = (2 * radius + 1) * (2 * radius + 1);
 
-        for (int y = 0; y < input.getHeight(); ++y) {
-            for (int x = 0; x < input.getWidth(); ++x) {
+        // iterate through each pixel of original image
+        for (int y = 0; y < input.getHeight(); y++) {
+            for (int x = 0; x < input.getWidth(); x++) {
 
                 int[] neighbourhoodValuesA = new int[size];
                 int[] neighbourhoodValuesR = new int[size];
                 int[] neighbourhoodValuesG = new int[size];
                 int[] neighbourhoodValuesB = new int[size];
 
-                for (int i = -radius, count = 0; i <= radius; i++) {
-                    for (int j = -radius; j <= radius; j++, count++) {
-                        int xVal = x + j;
-                        int yVal = y + i;
+                // iterate through neighbourhood
+                for (int yOffset = -radius, count = 0; yOffset <= radius; yOffset++) {
+                    for (int xOffset = -radius; xOffset <= radius; xOffset++, count++) {
+                        // x, y for neighbour
+                        int xVal = x + xOffset;
+                        int yVal = y + yOffset;
 
-                        // check for x,y outside width,height
+                        // bounds check
                         if (xVal < 0) {
                             xVal = 0;
                         } else if (xVal >= input.getWidth()) {
@@ -45,6 +104,7 @@ public class MedianFilter implements ImageOperation, java.io.Serializable {
                             yVal = input.getHeight() - 1;
                         }
 
+                        // unpack and store argb
                         int argb = input.getRGB(xVal, yVal);
                         int a = (argb & 0xFF000000) >> 24;
                         int r = (argb & 0x00FF0000) >> 16;
@@ -58,6 +118,7 @@ public class MedianFilter implements ImageOperation, java.io.Serializable {
                     }
                 }
 
+                // get medians
                 Arrays.sort(neighbourhoodValuesA);
                 Arrays.sort(neighbourhoodValuesR);
                 Arrays.sort(neighbourhoodValuesG);
@@ -68,6 +129,7 @@ public class MedianFilter implements ImageOperation, java.io.Serializable {
                 int medianG = neighbourhoodValuesG[size / 2];
                 int medianB = neighbourhoodValuesB[size / 2];
 
+                // pack argb and set in output
                 int argb = (medianA << 24) | (medianR << 16) | (medianG << 8) | medianB;
 
                 output.setRGB(x, y, argb);
