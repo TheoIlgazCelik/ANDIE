f1180ce0cf9803664dd867b28ccb116b2d966139
Ilgaz Celik <celil712@student.otago.ac.nz>
2024-04-12 13:44:32 +1200

    creating skeleton code for brightness and contrast

--- src/cosc202/andie/ColourActions.java
+++ src/cosc202/andie/ColourActions.java
@@ -40,6 +40,7 @@ public class ColourActions {
         actions.add(new ConvertToGreyAction("Greyscale", null, "Convert to greyscale", Integer.valueOf(KeyEvent.VK_G)));
         actions.add(new ColorInversionAction("Invert Color", null, "Invert image color", null));
         actions.add(new ColourCycleAction("Colour Channel Cycle", null, "Change Colour Cycle", null));
+        actions.add(new BrightnessAndContrast("Brightness and Contrast", null, "Adjust Brightness and Contrast", null));
     }
 
     /**
@@ -57,6 +58,90 @@ public class ColourActions {
         }
 
         return fileMenu;
+    }
+
+    private int getPercentageFromUser(int MIN_VALUE, int MAX_VALUE) {
+        int percentage = 100;
+
+        // JSpinner with range (MIN_VALUE - MAX_VALUE) inclusive
+        SpinnerNumberModel percentageModel = new SpinnerNumberModel(1, MIN_VALUE, MAX_VALUE, 1);
+        JSpinner percentageSpinner = new JSpinner(percentageModel);
+        // disable keyboard input
+        ((JSpinner.DefaultEditor) percentageSpinner.getEditor()).getTextField().setEditable(false);
+
+        ResourceBundle b = ResourceBundle.getBundle("cosc202.andie.LanguageBundle", Andie.locale);
+        Object[] options2 = { b.getString("Ok"), b.getString("Cancel") };
+        String optionMessage = b.getString("Percentage") + " (" + MIN_VALUE + " - " + MAX_VALUE + ")";
+
+        // Pop-up dialog box to ask for the radius value.
+        int option = JOptionPane.showOptionDialog(null, percentageSpinner, optionMessage,
+                JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options2, null);
+
+        // Check the return value from the dialog box.
+        // Ok = 0, Cancel = 1, Exit = -1
+        if (option == 0) {
+            percentage = percentageModel.getNumber().intValue();
+        }
+
+        return percentage;
+    }
+
+    /**
+     * <p>
+     * Action to blur an image with a Gaussian Blur
+     * </p>
+     * 
+     */
+    public class BrightnessAndContrast extends ImageAction {
+        private final int MIN_VALUE = 1;
+        private final int MAX_VALUE = 10;
+
+        /**
+         * <p>
+         * Create a new mean-filter action.
+         * </p>
+         * 
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+         */
+        BrightnessAndContrast(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        /**
+         * <p>
+         * Callback for when the convert-to-grey action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the GaussianBlurAction is triggered.
+         * It prompts the user for a filter radius, then applies an appropriately sized
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+            int radius = getPercentageFromUser(this.MIN_VALUE, this.MAX_VALUE);
+
+            if (radius <= 0) {
+                return;
+            }
+
+            // Create and apply the filter
+            try {
+                target.getImage().apply(new GaussianBlur(radius));
+                target.repaint();
+                target.getParent().revalidate();
+            } catch (Exception ex) {
+                ResourceBundle b = ResourceBundle.getBundle("cosc202.andie.LanguageBundle", Andie.locale);
+                Object[] options = { b.getString("Ok") };
+                JOptionPane.showOptionDialog(target, b.getString("No_image"), "Error", JOptionPane.CANCEL_OPTION,
+                        JOptionPane.ERROR_MESSAGE, null, options, null);
+            }
+        }
+
     }
 
     /**
@@ -81,7 +166,6 @@ public class ColourActions {
         ConvertToGreyAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
-
         /**
          * <p>
          * Callback for when the convert-to-grey action is triggered.
