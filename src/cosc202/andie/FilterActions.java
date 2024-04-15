5caec2612d1b3f7332d6515099cf3b63be87cb72
obrai798 <obrai798@student.otago.ac.nz>
2024-04-15 13:42:54 +1200

    Block average completed

--- src/cosc202/andie/FilterActions.java
+++ src/cosc202/andie/FilterActions.java
@@ -41,6 +41,7 @@ public class FilterActions {
         actions.add(new SharpenFilterAction("Sharpen filter", null, "Apply a sharpen filter", Integer.valueOf(KeyEvent.VK_M)));
         actions.add(new MedianFilterAction("Median Filter", null, "Apply a median filter", Integer.valueOf(KeyEvent.VK_M)));
         actions.add(new GaussianBlurAction("Gaussian Blur", null, "Apply a Gaussian Blur", Integer.valueOf(KeyEvent.VK_G)));
+        actions.add(new BlockAverageAction("Block Average",null,"Apply a block averaging filter",Integer.valueOf(KeyEvent.VK_M)));
     }
 
     /**
@@ -94,6 +95,45 @@ public class FilterActions {
         }
 
         return radius;
+    }
+
+    /**
+     * <p>
+     * Get filter radius x and y from user using JOptionPane opton dialog box.
+     * </p>
+     * 
+     * @param MIN_VALUE minimum bound for radius (inclusive)
+     * @param MAX_VALUE maximum bound for radius (inclusive)
+     * 
+     * @return The radius input from user. -1 when input invalid
+     */
+    private int[] getXYFromUser(int MIN_VALUE, int MAX_VALUE) {
+        int x = -1;
+        int y = -1;
+
+        // JSpinner with range (MIN_VALUE - MAX_VALUE) inclusive
+        SpinnerNumberModel radiusModel = new SpinnerNumberModel(1, MIN_VALUE, MAX_VALUE, 1);
+        SpinnerNumberModel radiusModel2 = new SpinnerNumberModel(1, MIN_VALUE, MAX_VALUE, 1);
+        JSpinner[] r = new JSpinner[2];
+        r[0] = new JSpinner(radiusModel);
+        r[1] = new JSpinner(radiusModel2);
+
+        ResourceBundle b = ResourceBundle.getBundle("cosc202.andie.LanguageBundle", Andie.locale);
+        Object[] options2 = { b.getString("Ok"), b.getString("Cancel") };
+        String optionMessage = b.getString("Filter_radius") + " (" + MIN_VALUE + " - " + MAX_VALUE + ")";
+
+        // Pop-up dialog box to ask for the radius value.
+        int option = JOptionPane.showOptionDialog(null, r, optionMessage,
+                JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options2, null);
+
+        // Check the return value from the dialog box.
+        // Ok = 0, Cancel = 1, Exit = -1
+        if (option == 0) {
+            x = radiusModel.getNumber().intValue();
+            y = radiusModel2.getNumber().intValue();
+        }
+
+        return new int[] {x,y};
     }
 
     /**
@@ -255,7 +295,7 @@ public class FilterActions {
 
             if (radius <= 0) {
                 return;
-            }
+            } 
 
             // Create and apply the filter
             try {
@@ -310,4 +350,61 @@ public class FilterActions {
         }
     }
 
+        /**
+     * <p>
+     * Action to blur an image with a block averaging filter.
+     * </p>
+     * 
+     * @see BlockAverage
+     */
+    public class BlockAverageAction extends ImageAction {
+        private final int MIN_VALUE = 1;
+        private final int MAX_VALUE = 20;
+
+        /**
+         * <p>
+         * Create a new block average filter action.
+         * </p>
+         * 
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+         */
+        BlockAverageAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        /**
+         * <p>
+         * Callback for when the block average filter action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the blockAverageAction is triggered.
+         * It prompts the user for a filter radius, then applies an appropriately sized
+         * {@link BlockAverage}.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+            int[] radius = getXYFromUser(this.MIN_VALUE, this.MAX_VALUE);
+
+            if (radius[0] <= 0 || radius[1] <= 0) {
+                return;
+            }
+
+            try {
+                target.getImage().apply(new BlockAverage(radius[0],radius[1]));
+                target.repaint();
+                target.getParent().revalidate();
+            } catch (Exception ex) {
+                ResourceBundle b = ResourceBundle.getBundle("cosc202.andie.LanguageBundle", Andie.locale);
+                Object[] options = { b.getString("Ok") };
+                JOptionPane.showOptionDialog(target, b.getString("No_image"), "Error", JOptionPane.CANCEL_OPTION,
+                        JOptionPane.ERROR_MESSAGE, null, options, null);
+            }
+        }
+    }
 }
