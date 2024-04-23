ff2b78c415ee5b07c962aa38cdd9b679c2f1bc0f
Ilgaz Celik <celil712@student.otago.ac.nz>
2024-04-23 21:17:39 +1200

    Fixed Gaussian Testing & created JUnit features

--- test/cosc202/andie/cosc202/andie/GaussianBlurTest.java
+++ test/cosc202/andie/cosc202/andie/GaussianBlurTest.java
@@ -1,12 +1,14 @@
-package test;
+package cosc202.andie.cosc202.andie;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import java.awt.image.*;
 import java.util.Arrays;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 import cosc202.andie.GaussianBlur;
-import cosc202.andie.ImagePanel;
+//import cosc202.andie.ImagePanel;
 
 public class GaussianBlurTest {
     
@@ -15,10 +17,13 @@ public class GaussianBlurTest {
         BufferedImage tester = new BufferedImage(10,10,10);
         GaussianBlur testBlur = new GaussianBlur(1);
         testBlur.apply(tester);
-        System.out.println(Arrays.toString(testBlur.array));
-        float[] compare = {0.0f,0.01f,0.0f,0.01f,0.957f,0.01f,0.0f,0.01f,0.0f};
-
-        Assertions.assertEquals(compare, testBlur.array);
+        NumberFormat df = new DecimalFormat("0.000");
+        double[] nums = new double[testBlur.array.length];
+        double[] compare = {0.000,0.011,0.000,0.011,0.957,0.011,0.000,0.011,0.000};
+        for (int i = 0;i<nums.length;i++){
+            nums[i]=Double.parseDouble(df.format(testBlur.array[i]));
+        }
+        Assertions.assertArrayEquals(compare, nums);
 
     }
 }
