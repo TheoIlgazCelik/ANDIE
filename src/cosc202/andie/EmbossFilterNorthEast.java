db0ff5859cd21e5e1cd55500ea96cd43a2f1e553
obrai798 <obrai798@student.otago.ac.nz>
2024-05-14 10:17:05 +1200

    Added javadoc comments to emboss filters

--- src/cosc202/andie/EmbossFilterNorthEast.java
+++ src/cosc202/andie/EmbossFilterNorthEast.java
@@ -1,9 +1,31 @@
 package cosc202.andie;
 
 import java.awt.image.*;
+/**
+ * <p>
+ * ImageOperation to apply a north east emboss filter.
+ * </p>
+ * 
+ * 
+ * @see java.awt.image.ConvolveOp
+ * @author 
+ * @version 1.0
+ */
 
 public class EmbossFilterNorthEast implements ImageOperation, java.io.Serializable{
 
+    /**
+     * <p>
+     * Apply an emboss filter to an image.
+     * </p>
+     * 
+     * <p>
+     * As with many filters, the emboss filter is implemented via convolution.
+     * </p>
+     * 
+     * @param input The image to apply the emboss filter to.
+     * @return The resulting (blurred)) image.
+     */
     public BufferedImage apply(BufferedImage input){
         float[] array = {1,0,0,0,0,0,0,0,-1};
         Kernel kernel = new Kernel(3,3,array);
--- src/cosc202/andie/EmbossFilterSouthEast.java
+++ src/cosc202/andie/EmbossFilterSouthEast.java
@@ -1,9 +1,31 @@
 package cosc202.andie;
 
 import java.awt.image.*;
+/**
+ * <p>
+ * ImageOperation to apply a south east emboss filter.
+ * </p>
+ * 
+ * 
+ * @see java.awt.image.ConvolveOp
+ * @author 
+ * @version 1.0
+ */
 
 public class EmbossFilterSouthEast implements ImageOperation, java.io.Serializable{
 
+    /**
+     * <p>
+     * Apply an emboss filter to an image.
+     * </p>
+     * 
+     * <p>
+     * As with many filters, the emboss filter is implemented via convolution.
+     * </p>
+     * 
+     * @param input The image to apply the emboss filter to.
+     * @return The resulting (blurred)) image.
+     */
     public BufferedImage apply(BufferedImage input){
         float[] array = {0,0,-1,0,0,0,1,0,0};
         Kernel kernel = new Kernel(3,3,array);
